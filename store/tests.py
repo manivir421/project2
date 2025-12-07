5f12ca24f3d9e950e67c2128d89dc007e3b851ab
manivir421 <manivir.3201@gmail.com>
2025-12-07 14:11:08 -0800

    third comit

--- store/tests.py
+++ store/tests.py
@@ -1,13 +1,32 @@
-
 from django.test import TestCase
+from com.shop.models import Product, Order
+from com.shop.recommendation_system import recommend_related_products
 
 class RecommendationSystemTest(TestCase):
 
+    def setUp(self):
+
+        self.product1 = Product.objects.create(name="Product 1")
+        self.product2 = Product.objects.create(name="Product 2")
+        self.product3 = Product.objects.create(name="Product 3")
+
+  
+        Order.objects.create(product=self.product1)
+        Order.objects.create(product=self.product2)
+
+
     def test_recommend_related_products_basic(self):
-         self.assertEqual(1, 0)
+
+        related = recommend_related_products(self.product1.id)
+        self.assertIn(self.product2, related)
+        self.assertNotIn(self.product1, related)
 
     def test_recommend_related_products_no_orders(self):
-         self.assertEqual(1, 0)
+
+        related = recommend_related_products(self.product3.id)
+        self.assertEqual(list(related), [])
 
     def test_recommend_related_products_top_n(self):
-        self.assertEqual(1, 0)
+
+        related = recommend_related_products(self.product1.id, top_n=1)
+        self.assertEqual(len(related), 1)
